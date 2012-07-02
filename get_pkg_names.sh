466452c21186bb83cbcfeac9a6007a2c7d7b05b0
davehome <davehome@redthumb.info.tm>
2012-07-02 05:08:04 -0600

    get_pkg_names.sh: only add pkgs that have been updated.

--- get_pkg_names.sh
+++ get_pkg_names.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
 
-PKGS="$*"
+__PKGS="${*}"
 
 rm -f repo-checkvers.txt
 touch repo-checkvers.txt
 
-for p in $PKGS; do
-	f=$(echo $p | grep srcpkgs)
-	if [ -n "$f" ]; then
-		dir=$(dirname $f)
-		pkg=$(basename $dir)
-		echo "- $pkg repover" >> repo-checkvers.txt
+for __p in ${__PKGS}; do
+	__f=$(echo ${__p} | grep srcpkgs)
+	if [ -n "${__f}" ]; then
+		. ${__f}
+		__ver="${version}"
+		__ver="${__ver}_${revision}"
+		__dir=$(dirname ${__f})
+		__pkg=$(basename ${__dir})
+		__repover="$(xbps-repo show -oversion ${__pkg})"
+		echo ${__pkg}
+		echo ${__ver}
+		echo ${__repover}
+		xbps-uhelper cmpver ${__repover} ${__ver}
+		if [ $? -gt 0 ]; then
+			echo "- ${__pkg} repover" >> repo-checkvers.txt
+		fi
 	fi
 done
