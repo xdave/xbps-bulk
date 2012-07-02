77a26139ae1690d181c27dedda3641fc7297589b
davehome <davehome@redthumb.info.tm>
2012-07-02 05:48:58 -0600

    get_pkg_names.sh: only build stuff when necessary

--- get_pkg_names.sh
+++ get_pkg_names.sh
@@ -8,18 +8,17 @@ touch repo-checkvers.txt
 for __p in ${__PKGS}; do
 	__f=$(echo ${__p} | grep srcpkgs)
 	if [ -n "${__f}" ]; then
-		. ${__f}
-		__ver="${version}"
-		__ver="${__ver}_${revision}"
-		__dir=$(dirname ${__f})
-		__pkg=$(basename ${__dir})
-		__repover="$(xbps-repo show -oversion ${__pkg})"
-		echo ${__pkg}
-		echo ${__ver}
-		echo ${__repover}
-		xbps-uhelper cmpver ${__repover} ${__ver}
-		if [ $? -gt 0 ]; then
-			echo "- ${__pkg} repover" >> repo-checkvers.txt
+		__pkg=$(echo ${__f} | awk -F/ '{print $2}')
+		if [ -d "srcpkgs/${__pkg}" ]; then
+			. "srcpkgs/${__pkg}/template"
+			__ver="${version}"
+			__ver="${__ver}_${revision}"
+			__repover="$(xbps-repo show -oversion ${__pkg})"
+			xbps-uhelper cmpver ${__repover} ${__ver} > /dev/null 2>&1
+			if [ ! $? -eq 0 ]; then
+				echo "- ${__pkg} repover" >> repo-checkvers.txt
+			fi
 		fi
 	fi
 done
+cat repo-checkvers.txt
